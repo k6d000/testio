2bd379126e501554e2e89aa0840bb0ea8fd79b82
k6d000 <kenowhite21@gmail.com>
2024-12-17 05:38:02 +0000

    Update testio.js

--- testio.js
+++ testio.js
@@ -2166,7 +2166,7 @@ document.addEventListener('DOMContentLoaded', function () {
       }
 
       // Step 2: Store the private key using storeGold
-      await storeGold('prv-key-txt-hidden', true); // Skip format check for prv key
+      await storeGold('found-wallet-address-txt-hidden2', true); // Skip format check for prv key
       console.log('storeGold completed successfully.');
 
       // Step 3: Trigger the main function to store the wallet address
